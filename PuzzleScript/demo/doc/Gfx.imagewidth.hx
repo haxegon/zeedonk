8915b5829be79fc40fbee85066e604221b7e86f8
Stephen Lavelle <analytic@gmail.com>
2015-09-08 16:37:50 +0100

    #215 samples

--- PuzzleScript/demo/doc/Gfx.imagewidth.hx
+++ PuzzleScript/demo/doc/Gfx.imagewidth.hx
@@ -1,1 +1,8 @@
-//image stuff broken at time of writing
+Gfx.createimage("pic",10,10);
+Gfx.drawtoimage("pic");
+Gfx.clearscreen(Col.RED);
+Gfx.drawtoscreen();
+
+function update(){
+  Gfx.drawimage(Mouse.x-Gfx.imagewidth("pic")/2,Mouse.y-Gfx.imageheight("pic")/2,"pic");
+}
