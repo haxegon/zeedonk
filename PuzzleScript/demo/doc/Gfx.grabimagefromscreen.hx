4d04df94c9e1a8b6694c6e1a86cd2ab60e5b754a
Stephen Lavelle <analytic@gmail.com>
2015-09-08 16:30:18 +0100

    sample for #215

--- PuzzleScript/demo/doc/Gfx.grabimagefromscreen.hx
+++ PuzzleScript/demo/doc/Gfx.grabimagefromscreen.hx
@@ -1,1 +1,8 @@
-//image stuff broken at time of writing
+Gfx.createimage("pic",Gfx.screenwidth,Gfx.screenheight);
+
+function update(){
+  Gfx.drawimage(-1,-1,"pic");
+  var c = Gfx.HSL(Game.time*10,1,0.5);
+  Gfx.drawcircle(Mouse.x,Mouse.y,10,c);
+  Gfx.grabimagefromscreen("pic",0,0);
+}
