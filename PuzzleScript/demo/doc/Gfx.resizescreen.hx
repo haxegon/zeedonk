312259e4cbabd0b522e7926695972bdc7d9b34d9
Stephen Lavelle <analytic@gmail.com>
2015-09-08 16:35:29 +0100

    tweaking Gfx.resizescreenexample (#215)

--- PuzzleScript/demo/doc/Gfx.resizescreen.hx
+++ PuzzleScript/demo/doc/Gfx.resizescreen.hx
@@ -1,8 +1,7 @@
-//broken at time of writing
 Game.background(Col.RED);
 var w = 100+50;
 var h = 100-50;
-Gfx.resizescreen(w,h);
+Gfx.resizescreen(w,h,5);
 function update(){
   Gfx.clearscreen(Col.BLUE);
   var t = Game.time/30;
