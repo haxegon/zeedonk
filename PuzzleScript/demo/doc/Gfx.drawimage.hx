1e2e21c31527d9c8dd5004167adfb93e356733b6
Stephen Lavelle <analytic@gmail.com>
2015-09-08 15:46:54 +0100

    stuff for #215

--- PuzzleScript/demo/doc/Gfx.drawimage.hx
+++ PuzzleScript/demo/doc/Gfx.drawimage.hx
@@ -1,9 +1,8 @@
-//this is broken. i don't know why
 Gfx.createimage("pic",30,30);
 Gfx.drawtoimage("pic");
 Gfx.fillbox(10,10,10,10,Col.RED);
 Gfx.drawtoscreen();
 
 function update(){
-  Gfx.drawimage(0,0,"pic");
+  Gfx.drawimage(Game.time%30,0,"pic");
 }
