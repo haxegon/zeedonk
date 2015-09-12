c4b61193ee40ea370676c9a9d1ce638fff3ae320
Terry Cavanagh <terry@maths.tcd.ie>
2015-09-12 12:08:53 +0100

    deleting old stuff

--- PuzzleScript/demo/ex4.hx
+++
@@ -1,8 +0,0 @@
-function new(){
-  Music.playnote(4123,1,1,1);
-}
-
-function update() {
-	Text.display(0, 0, "Hello, TerryLib!");
-}
-
--- PuzzleScript/demo/ex8.hx
+++
@@ -1,6 +0,0 @@
-function new(){
-}
-
-function update() {
-  Text.display(0, 0, "Hello World");
-}
