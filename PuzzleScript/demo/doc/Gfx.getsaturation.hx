feaaa1dec0ac246597dc8883303c5cb3a55d4448
Stephen Lavelle <analytic@gmail.com>
2015-09-14 21:45:21 +0100

    fixes #249

---
+++ PuzzleScript/demo/doc/Gfx.getsaturation.hx
@@ -0,0 +1,11 @@
+var c = 0x112233;
+trace(c.toString(16));
+var h = Gfx.gethue(c);
+var s = Gfx.getsaturation(c);
+var l = Gfx.getlightness(c);
+trace(h,s,l);
+//to convert them back
+var c2 = Gfx.HSL(h,s,l);
+//.toString(16) converts integers to strings, base 16
+trace(c2.toString(16));
+trace("close enough!");
