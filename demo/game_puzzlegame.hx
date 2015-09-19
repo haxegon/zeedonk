b091f9ac3d95e6cba3d6de5028a86f82f8d740c3
Terry Cavanagh <terry@maths.tcd.ie>
2015-09-19 22:31:23 +0100

    Fixed up Puzzle Game

--- demo/game_puzzlegame.hx
+++ demo/game_puzzlegame.hx
@@ -45,7 +45,7 @@ var start;
 var changerate;
 
 function new() {
-  Text.setfont("tall", 1);
+  Text.setfont(Font.THIN, 1);
   start = 0;
 
   sr = 0;
@@ -55,30 +55,30 @@ function new() {
   finishedyet = 0;
 
   content[0] = [ 
-    "At 8am every weekday, Mona catches the same train, which travels\nat an average of 48 kilometres an hour.", 
-    "When Mona leaves her apartment each morning, she takes the lift\nto the ground floor.",
+    "At 8am every weekday, Mona catches the same train, which\ntravels at an average of 48 kilometres an hour.", 
+    "When Mona leaves her apartment each morning, she takes \nthe lift to the ground floor.",
     "Mona puts one teaspoon of sugar in her tea each morning.",
-    "Mona has twenty-four pairs of socks. Some of them are grey, and\nsome of them are black.",
-    "Mona has two flatmates. One of them always lies, and the other\nalways tells the truth.",
+    "Mona has twenty-four pairs of socks. Some of them are \ngrey, and some of them are black.",
+    "Mona has two flatmates. One of them always lies, and the\nother always tells the truth.",
     "Each morning, Mona walks one mile south to the bus stop.",
-    "Mona packs herself lunch on one morning out of every three.",
-    "When Mona reaches her office in the morning, she catches the\nelevator to the tenth floor."];
-  content[1] = [ "Sometimes on the weekends, she goes for a walk to\nthe local market.", 
-                "She drinks instant coffee, which she buys in a small jar every\nfortnight.",
+    "Mona packs herself lunch on one morning out of every\nthree.",
+    "When Mona reaches her office in the morning, she catches\nthe elevator to the tenth floor."];
+  content[1] = [ "Sometimes on the weekends, she goes for a walk to the\nlocal market.", 
+                "She drinks instant coffee, which she buys in a small jar\nevery fortnight.",
                 "One day, she drops a table tennis ball down a hole.",
                 "On her desk there is an unopened package.",
                 "On the bathroom floor there is a pool of water.",
-                "Everyone at her workplace has either blue eyes or brown eyes.",
-                "For lunch she eats first one grain of rice, then two, then four,\nthen eight and so on.",
-                "She pays for her lunch with a note and three coins, and receives\nfour coins in return.",
-                "She runs along the corridor with a piece of paper in her hand.",
+                "Everyone at her workplace has either blue eyes or brown\neyes.",
+                "For lunch she eats first one grain of rice, then two, then\nfour, then eight and so on.",
+                "She pays for her lunch with a note and three coins, and\nreceives four coins in return.",
+                "She runs along the corridor with a piece of paper in her\nhand.",
                 "On her train journey, Mona reads the newspaper.",
-                "She has two water bottles. One of them holds 500 mililitres,\nand the other holds 600 mililitres.",
-                "There are three light switches outside her bathroom, but only one\nis connected to the bathroom light.",
-                "Mona's elder flatmate, Rhona, is twice the age that\nthe younger, Ilona, was two years ago."];
+                "She has two water bottles. One of them holds 500\nmililitres, and the other holds 600 mililitres.",
+                "There are three light switches outside her bathroom, but\nonly one is connected to the bathroom light.",
+                "Mona's elder flatmate, Rhona, is twice the age that the\nyounger, Ilona, was two years ago."];
   content[2] = [ "One day she finds Rhona lying dead outside her house. There\nare no signs of violence.", 
-                "Every day on her lunch break, Mona walks to a park near her\nworkplace.",
-                "One afternoon she looks out of the window and sees an unexpected\nbird.",
+                "Every day on her lunch break, Mona walks to a park near\nher workplace.",
+                "One afternoon she looks out of the window and sees an\nunexpected bird.",
                 "Every evening, Mona places two icecubes in a glass.",
                 "One evening she goes to the supermarket to buy a cabbage,\na bag of beans and a goose.",
                 "She picks up one black pebble on the ground, and one\nwhite pebble.",
@@ -86,8 +86,8 @@ function new() {
                 "As she walks, she crosses each bridge exactly once.",
                 "She does not know when Rhona's birthday is.",
                 "Every afternoon at 4:15, Mona puts her phone on to charge.",
-                "While walking home one Tuesday evening, she finds a\nbroken straw on the ground.", 
-                "One night in late summer she finds a pool of water on\nthe path and walks through, not knowing where it came from."];
+                "While walking home one Tuesday evening, she finds a broken\nstraw on the ground.", 
+                "One night in late summer she finds a pool of water on\nthe path and walks through."];
   content[3] = [ "When she returns home, she walks up the stairs.", 
                 "In the evening, she walks one mile east.",
                 "On her way home she passes a branch in the road.",
@@ -97,13 +97,13 @@ function new() {
                 "She walks up the final three flights of stairs.",
                 "Most nights at 2am, she lies awake for a little while.",
                 "Every evening, she gets into bed, winds up her alarm clock,\nand sets it for 7am the next morning.", 
-                "For dinner she cooks scrambled eggs. She has twelve eggs of\ndifferent weights, which appear to be identical.",
+                "For dinner she cooks scrambled eggs. She has twelve eggs\nof different weights, which appear to be identical.",
                 "She knows that her boyfriend is cheating on her.",
                 "Late at night, she puts on her hat without looking at it.",
                 "As she walks through the park, she sees a carrot lying\non the ground.",
-                "She takes out a gun and points it at the man, who thanks her."];
+                "She takes out a gun and points it at the man, who\nthanks her."];
   content[4] = [ "Why?", 
-                "Assume that Mona is perfectly logical. Does she change her mind?",
+                "Assume that Mona is perfectly logical. Does she change\nher mind?",
                 "When does Mona die?",
                 "How?",
                 "Where did the extra dollar go?",
@@ -177,9 +177,9 @@ function update() {
       Gfx.fillbox(0, line[i], Gfx.screenwidth, lineheight, Col.BLACK);
     }
 
-    Text.display(Gfx.screenwidthmid, box[1] + 10, "PUZZLE GAME", startcolour, {align: Text.CENTER});
-    Text.display(Gfx.screenwidthmid, box[2] + 10, "use arrow keys to play", lightblack, {align: Text.CENTER});
-    Text.display(Gfx.screenwidthmid, box[3] + 10, "Holly Gramazio", lightblack, { align: Text.CENTER } );
+    Text.display(Gfx.screenwidthmid, box[1] + 12, "PUZZLE GAME", startcolour, {align: Text.CENTER});
+    Text.display(Gfx.screenwidthmid, box[2] + 12, "use arrow keys to play", lightblack, {align: Text.CENTER});
+    Text.display(Gfx.screenwidthmid, box[3] + 12, "Holly Gramazio", lightblack, { align: Text.CENTER } );
 
     if (Input.justpressed(Key.DOWN) || Input.justpressed(Key.UP) || Input.justpressed(Key.LEFT) || Input.justpressed(Key.RIGHT))
     {
@@ -196,8 +196,8 @@ function update() {
   else if (greydupeb <= 20)
   {
     Gfx.fillbox(0, 0, Gfx.screenwidth, 62, Col.GREY);
-    Gfx.fillbox(0, 27, Gfx.screenwidth, 94, Col.BLUE);
-    Gfx.fillbox(0, 125, Gfx.screenwidth, 94, Col.GREY);
+    Gfx.fillbox(0, 27, Gfx.screenwidth, 110, Col.BLUE);
+    Gfx.fillbox(0, 137, Gfx.screenwidth, 94, Col.GREY);
 
     story = "";
     for (i in 0 ... 4) {
@@ -214,7 +214,7 @@ function update() {
       Gfx.fillbox(0, box[i], Gfx.screenwidth, boxheight, greydupe);
       Gfx.fillbox(0, line[i], Gfx.screenwidth, lineheight, Col.BLACK);
 
-      Text.display(5, box[i] + 2, content[i][contentat[i]], Col.BLACK);
+      Text.display(5, box[i] + 5, content[i][contentat[i]], Col.BLACK);
     }
 
     greyduper -= 2;
