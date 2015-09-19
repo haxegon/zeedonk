f6098c55dd8fdae4ffcae5b7fc8673533837afee
Stephen Lavelle <analytic@gmail.com>
2015-09-19 23:29:29 +0100

    fixes #314

--- haxegonweb/src/Main.hx
+++ haxegonweb/src/Main.hx
@@ -27,7 +27,7 @@ class Webbridge {
 	}
 	
 	public function stop() {
-		Gfx.resizescreen(240, 150);
+		Gfx.resizescreen(192, 120, 4);
 		Text.setfont("default", 1);
 		Webscript.scriptloaded = false;
 		Webscript.runscript = false;
