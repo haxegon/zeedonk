dfb7fd855640c225c354f86053d1d955f35228aa
Terry Cavanagh <terry@maths.tcd.ie>
2015-09-24 21:38:16 +0100

    Fixed scrolling bug with play.html

--- js/lime_embed.js
+++ js/lime_embed.js
@@ -1,3 +1,10 @@
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 var r = window.devicePixelRatio;
 var w = Math.round(window.innerWidth * r);
 var h = Math.round(window.innerHeight * r);
