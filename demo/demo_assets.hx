fecdb609e9458fe2f5bd02695baa4dc1dce2f234
Terry Cavanagh <terry@maths.tcd.ie>
2015-09-29 23:53:50 +0100

    Working on new example, #366

---
+++ demo/demo_assets.hx
@@ -0,0 +1,198 @@
+/** 
+  This is a free pack of graphical assets you can use in your own zeedonk games!
+  Hope you find it useful!
+
+  All of these sprites and icons where created with the Zeedonk Sprite editor,
+  which you can access from the dropdown examples list.  - Terry
+**/
+
+// ASSETS START HERE
+Gfx.loadimagestring("tile0","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile1","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile2","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile3","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile4","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile5","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile6","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile7","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile8","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile9","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile10","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile11","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile12","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile13","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile14","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile15","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile16","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile17","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile18","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile19","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile20","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile21","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile22","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile23","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile24","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile25","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile26","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile27","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile28","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile29","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile30","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile31","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile32","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile33","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile34","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile35","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile36","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile37","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile38","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile39","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile40","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile41","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile42","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile43","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile44","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile45","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile46","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile47","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile48","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile49","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile50","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile51","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile52","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile53","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile54","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+Gfx.loadimagestring("tile55","ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6");
+
+Gfx.loadimagestring("icon0", "3SaaapZZZYRNCuNmCxpH6"); // Mine
+Gfx.loadimagestring("icon1", "3SaaapZZZSaZCcYJFcQHa"); // Corner, Top left
+Gfx.loadimagestring("icon2", "3SaaapZZZSbZaxPXdwOHa"); // Corner, Top right
+Gfx.loadimagestring("icon3", "3SaaapZZZUOHFcYJCdYaa"); // Corner, Bottom left
+Gfx.loadimagestring("icon4", "3SaaapZZZUPHdxPXaxWaa"); // Corner, Bottom right
+Gfx.loadimagestring("icon5", "3SaaapZZZSbZ6hZZ9gQHa"); // Up
+Gfx.loadimagestring("icon6", "3SaaapZZZUPH9hZZ6hYaa"); // Down
+Gfx.loadimagestring("icon7", "3SaaapZZZUOHFcYJFcQHa"); // Left
+Gfx.loadimagestring("icon8", "3SaaapZZZUPHdxPXdwOHa"); // Right
+Gfx.loadimagestring("icon9", "3SaaapZZZSbZ6hZZ6hYaa"); // Horizontal
+Gfx.loadimagestring("icon10", "3SaaapZZZUOHlsOHlsOHa"); // Vertical
+Gfx.loadimagestring("icon11", "3SaaapZZZUPH9hZZ9gQHa"); // Cross
+Gfx.loadimagestring("icon12", "3SaaapZZZZZaKmdaKmdZ6"); // Square
+Gfx.loadimagestring("icon13", "3SaaapZZZS2hbaqWpdKSa"); // Music Note
+Gfx.loadimagestring("icon14", "3SaaapZZZSK4pRY4h7Saa"); // Up arrow
+Gfx.loadimagestring("icon15", "3SaaapZZZSa4h7TZJLSma"); // Down arrow
+Gfx.loadimagestring("icon16", "3SaaapZZZS6ypRXZpL6ia"); // Left arrow
+Gfx.loadimagestring("icon17", "3SaaapZZZSqgpNYZJKyea"); // Right arrow
+Gfx.loadimagestring("icon18", "3SaaapZZZSa4mMOHmLSaa"); // Radio button, selected
+Gfx.loadimagestring("icon19", "3SaaapZZZSa4mMe7mLSaa"); // Radio button, unselected
+Gfx.loadimagestring("icon20", "3SaaapZZZVnZZZZZJLSma"); // Heart
+Gfx.loadimagestring("icon21", "3SaaapZZZSK4pRZZJLSma"); // Diamond
+Gfx.loadimagestring("icon22", "3SaaapZZZSK4pRZZJKK4a"); // Spade
+Gfx.loadimagestring("icon23", "3SaaapZZZSK4hARZZYQma"); // Club
+Gfx.loadimagestring("icon24", "3SaaapZZZVXZV5Z7WZYZa"); // Face, filled
+Gfx.loadimagestring("icon25", "3SaaapZZZVXaOGd4NmcZa"); // Face, outlined
+Gfx.loadimagestring("icon26", "3SaaapZZZWqaaabeaaaaa"); // Dither, 1
+Gfx.loadimagestring("icon27", "3SaaapZZZWqaeqbeabeaa"); // Dither, 2
+Gfx.loadimagestring("icon28", "3SaaapZZZXuaeqbvabeaa"); // Dither, 3
+Gfx.loadimagestring("icon29", "3SaaapZZZXuavqbvafuaa"); // Dither, 4
+Gfx.loadimagestring("icon30", "3SaaapZZZXu8vqbvi=uaa"); // Dither, 5
+Gfx.loadimagestring("icon31", "3SaaapZZZXu8vq9vi=ui6"); // Dither, 6
+Gfx.loadimagestring("icon32", "3SaaapZZZXuEPq9vkHui6"); // Dither, 7
+Gfx.loadimagestring("icon33", "3SaaapZZZXuEPsFvkHuE6"); // Dither, 8
+Gfx.loadimagestring("icon34", "3SaaapZZZZ2EPsFRkHuE6"); // Dither, 9
+Gfx.loadimagestring("icon35", "3SaaapZZZZ2ER0FRkHQE6"); // Dither, 10
+Gfx.loadimagestring("icon36", "3SaaapZZZZYER0FZEHQE6"); // Dither, 11
+Gfx.loadimagestring("icon37", "3SaaapZZZZYEZUFZEJYE6"); // Dither, 12
+Gfx.loadimagestring("icon38", "3SaaapZZZZZIZUFZVJYE6"); // Dither, 13
+Gfx.loadimagestring("icon39", "3SaaapZZZZZIZVJZVJYV6"); // Dither, 14
+Gfx.loadimagestring("icon40", "3SaaapZZZZZZZVJZZZYV6"); // Dither, 15
+Gfx.loadimagestring("icon41", "3KaaapZZZZaRL5aRL0EEEEEEEEEEEEEEEEEEEEE"); // Full
+Gfx.loadimagestring("icon42", "3SaaapZZZVWZadWZadWZa"); // Three, Horizontal
+Gfx.loadimagestring("icon43", "3SaaapZZZSbHV4RHV4Qaa"); // Three, Vertical
+Gfx.loadimagestring("icon44", "3SaaapZZZSaZpKaapNWaa"); // Two, Horizontal
+Gfx.loadimagestring("icon45", "3SaaapZZZSaNmNmNmNmaa"); // Two, Vertical
+Gfx.loadimagestring("icon46", "3SaaapZZZSaaadWZaaaaa"); // One, Horizontal
+Gfx.loadimagestring("icon47", "3SaaapZZZSamdaKmdaKaa"); // One, Vertical
+Gfx.loadimagestring("icon48", "3SaaapZZZSgd7KT24dbya"); // Sword
+Gfx.loadimagestring("icon49", "3SaaapZZZZZaRGNmS1m4a"); // Shield
+Gfx.loadimagestring("icon50", "3SaaapZZZS2n6yogdb6Ka"); // Staff
+Gfx.loadimagestring("icon51", "3SaaapZZZSSsiBY8i7ioa"); // Bow
+Gfx.loadimagestring("icon52", "3SaaapZZZSahbK2hhNWaa"); // Boots
+Gfx.loadimagestring("icon53", "3SaaapZZZSnB59ithVAKa"); // Throwing star
+Gfx.loadimagestring("icon54", "3SaaapZZZSa7ltWZitWaa"); // Helmet
+Gfx.loadimagestring("icon55", "3SaaapZZZSbNZZYNh9Waa"); // Armor
+Gfx.loadimagestring("icon56", "3SaaapZZZSaZpNmNmNmaa"); // Trousers
+Gfx.loadimagestring("icon57", "3SaaapZZZTSLjkEEEk26a"); // Whip
+Gfx.loadimagestring("icon58", "3SaaapZZZWd7ZZZHNiKma"); // Axe
+Gfx.loadimagestring("icon59", "3SaaapZZZSaZpNWmdaKaa"); // Hammer
+Gfx.loadimagestring("icon60", "3SaaapZZZSaahsi8i7Oaa"); // Alpha
+Gfx.loadimagestring("icon61", "3SaaapZZZTK8i9K8isSaa"); // Beta
+Gfx.loadimagestring("icon62", "3SaaapZZZSaaJPise7iaa"); // Pi
+Gfx.loadimagestring("icon63", "3SaaapZZZVWLgaKymtWaa"); // Sigma
+Gfx.loadimagestring("icon64", "3SaaapZZZSaahMi8i7Kaa"); // Sigma, lower
+Gfx.loadimagestring("icon65", "3SaaapZZZSa4ise7e9maa"); // Omega
+Gfx.loadimagestring("icon66", "3SaaapZZZTSNitWZitm4a"); // Theta
+Gfx.loadimagestring("icon67", "3SaaapZZZUeHlsOZh6Kma"); // Psi
+Gfx.loadimagestring("icon68", "3SaaapZZZSK4lsOHlrSma"); // Phi
+Gfx.loadimagestring("icon69", "3SaaapZZZTWKgaStitm4a"); // Delta
+Gfx.loadimagestring("icon70", "3SaaapZZZSaKwaKogNgaa"); // Lambda
+Gfx.loadimagestring("icon71", "3SaaapZZZSaNmNmNoNyKa"); // Mu
+Gfx.loadimagestring("icon72", "3SaaapZZZSK4f7isg7Sma"); // Crystal 1
+Gfx.loadimagestring("icon73", "3SaaapZZZS62nA2TAKSea"); // Crystal 2
+Gfx.loadimagestring("icon74", "3SaaapZZZSK7dfls9cema"); // Crystal 3
+Gfx.loadimagestring("icon75", "3SaaapZZZSasddmNdbiaa"); // Crystal 4
+Gfx.loadimagestring("icon76", "3SaaapZZZSa4nNOJoLSaa"); // Crystal 5
+Gfx.loadimagestring("icon77", "3SaaapZZZSy44QmLHTSya"); // Crystal 6
+Gfx.loadimagestring("icon78", "3SaaapZZZTSNmLSehaq2a"); // Key 1
+Gfx.loadimagestring("icon79", "3SaaapZZZUeZpLSehaq2a"); // Key 2
+Gfx.loadimagestring("icon80", "3SaaapZZZTSNise7jse7a"); // Door 1
+Gfx.loadimagestring("icon81", "3SaaapZZZTSZpNWZptWZa"); // Door 2
+Gfx.loadimagestring("icon82", "3SaaapZZZSa4pKaZmNWaa"); // Chest
+Gfx.loadimagestring("icon83", "3SaaapZZZSa4h7SmdaKaa"); // Lock
+Gfx.loadimagestring("icon84", "3SaaapZZZSacaKqimbaaa"); // Bone
+Gfx.loadimagestring("icon85", "3SaaapZZZSa4pMGFp7Kaa"); // Skull
+Gfx.loadimagestring("icon86", "3SaaapZZZYhNZXVmZYR76"); // Monster
+Gfx.loadimagestring("icon87", "3SaaapZZZSa4pMOZmPkHa"); // Monster 2
+Gfx.loadimagestring("icon88", "3SaaapZZZTSZltWZpNWHa"); // Ghost
+Gfx.loadimagestring("icon89", "3SaaapZZZSb5RXYa4ZJV6"); // Wall
+
+Gfx.loadimagestring("16r", "ZSaaan=I3ZZZKadybHacKadaamaaKadaamaaKadaanacQa1aapZZ6", Col.TRANSPARENT, Col.RED);
+
+var tile = [];
+var icon = [];
+for(i in 0 ... 56) tile.push("tile"+i);
+for(i in 0 ... 90) icon.push("icon"+i);
+// ASSETS END HERE
+
+//Things to do:
+//Characters
+//Weapon icons & items
+//tiles: forest, town, indoors, castle
+//different puzzly blocks
+
+function new(){
+  Game.title = "Zeedonk Asset Pack";
+  Text.setfont(Font.THIN);
+}
+
+function update(){
+  Gfx.clearscreen(Col.NIGHTBLUE);
+  for(j in 0 ... 4){
+    for(i in 0 ... 10){
+      Gfx.drawimage(4 + i*16, j*16, "tile0");
+    }  
+  }
+  
+  for(j in 0 ... 4){
+    for(i in 0 ... 10){
+      Gfx.drawimage(4 + i*16, 72 + j*16, "16r");
+    }  
+  }
+  
+  for(j in 0 ... 15){
+    for(i in 0 ... 6){
+      Gfx.drawimage(176 + i*9, j*9, icon[i + (j * 6)]);
+    }  
+  }
+  Text.display(Text.LEFT + 5, Text.BOTTOM, "ZEEDONK ASSET PACK");
+  Text.display(Text.RIGHT - 5, Text.BOTTOM, "FEEL FREE TO USE IN YOUR OWN GAMES");
+}
